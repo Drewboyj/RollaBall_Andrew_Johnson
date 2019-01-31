0e26aa08c3e08dd953c0cf3a8d1a7492aadd11e4
Drewboyj <46984790+Drewboyj@users.noreply.github.com>
2019-01-31 14:18:06 -0500

    The Finished RollaBall Challenge files
    The full Unity files for RollaBall Challenge 1

---
+++ Roll a Ball Challenge/Assets/Scripts/MovingWall2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingWall2 : MonoBehaviour
+{
+    public float delta = 10f;  // Amount to move left and right from the start point
+    public float speed = 2.0f;
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.z += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
---
+++ Roll a Ball Challenge/Assets/Scripts/Playercontroller.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Playercontroller : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText;
+    public Text winText;
+    public Text livesText;
+      
+   
+    private Rigidbody rb;
+    private int count;
+    private int lives;
+    
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        lives = 3;
+        count = 0;
+        SetlivesText();
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+        if (Input.GetKey("escape"))
+            Application.Quit();
+           
+                  
+          
+        }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Bad Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            lives = lives - 1;
+            SetlivesText();
+        }
+
+        if (count == 12) 
+{
+    transform.position = new Vector3(35.0f,.0f,.0f); 
+}
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 20)
+        {
+            winText.text = "You Win!";
+            if (gameObject.tag == "Player")
+            {
+                Destroy(obj: gameObject);
+            }
+        }
+    }
+
+    void SetlivesText()
+    {
+        livesText.text = "lives: " + lives.ToString();
+        bool v = lives <= 0;
+        if (v)
+        {
+            winText.text = "You Lose.";
+            if (gameObject.tag == "Player")
+            {
+                Destroy(obj: gameObject);
+            }
+
+        }
+
+
+    }
+
+}
